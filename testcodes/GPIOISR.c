544f20a0f104f71f3d6caa7846b594f0e8a207e2
RalfvTrigt <0944735@hr.nl>
2023-01-26 14:08:20 +0000

    ISR testcode complete

--- testcodes/GPIOISR.c
+++ testcodes/GPIOISR.c
@@ -4,25 +4,41 @@
 #include <stdbool.h>
 #include <wiringPi.h>
 
-#define MAGNET_SENSOR 14;
+#define MAGNET_SENSOR 23
+#define DOOR_LED 24
 
 volatile bool doorOpen;
 
 void magnetRead(){
-	if(digitalRead(MAGNET_SENSOR){
+	if(digitalRead(MAGNET_SENSOR)){
 		doorOpen = true;
 	}
-	else if(!digitalRead(MAGNET_SENSOR){
+	else if(!digitalRead(MAGNET_SENSOR)){
 		doorOpen = false;
 	}
 }
 
 int main(int argc, char* argv[]){
-	wiringPiSetup();
+	wiringPiSetupGpio();
 	pinMode(MAGNET_SENSOR, INPUT);
-	pullUpDnControl(MANGET_CONTROL, PUD_UP);
+	digitalWrite(MAGNET_SENSOR, 1);
+	pullUpDnControl(MAGNET_SENSOR, PUD_UP);
 	
 	wiringPiISR(MAGNET_SENSOR, INT_EDGE_BOTH, magnetRead);
+	
+	pinMode(DOOR_LED, OUTPUT);
+	digitalWrite(DOOR_LED, 0);
+	
+	magnetRead();
+	
+	while(1){
+		if (doorOpen == true){
+			digitalWrite(DOOR_LED, 1);
+		}
+		else if (doorOpen == false){
+			digitalWrite(DOOR_LED, 0);
+		}
+	}
 	
 	return 0;
 }
