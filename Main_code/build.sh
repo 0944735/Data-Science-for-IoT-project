32c88efe90684dba2b262c7718a880acbdb17f51
RalfvTrigt <0944735@hr.nl>
2023-01-26 12:09:46 +0000

    setup for main

---
+++ Main_code/build.sh
@@ -0,0 +1,1 @@
+gcc -o main.o main.c -lpaho-mqtt3c -lwiringPi
---
+++ Main_code/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <wiringPi.h>
+#include "MQTTClient.h"
+
+int main(int argc, char* argv[]){
+	return 0;
+}
